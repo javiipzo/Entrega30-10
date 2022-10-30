e219890c26f149c8a816bd9e22bac2c12cb4be6a
javiipzo <javipozo999@gmail.com>
2022-10-30 17:20:27 +0100

    nuevo

---
+++ generacionFrutas.py
@@ -0,0 +1,11 @@
+#---- IMPORTAR MÓDULOS --  
+import random
+import pandas as pnd
+
+#---- CARACTERÍSTICAS------  
+
+#CEREZAS 
+caracteristicasCerezas = [[17,19,1,5],[20,21,5,6],[22,23,6,7],[24,25,7,8.5],[26,27,8.5,10],[28,29,10,11.5]]
+
+#ALBARICOQUES  
+caracteristicasAlbaricoques = [[40,44,41],[45,49,54],[50,54,74],[55,59,100]] 
